4457c63995cc79840897dac088b8d3d87a5da8ac
palwashamuneeb <palwashasheikh73@gmail.com>
2024-10-10 16:44:12 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,124 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(Myapp());
+// }
+
+// class Myapp extends StatelessWidget {
+//   const Myapp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Center(child: Text('This is my First App')),
+//         ),
+//         body: Container(
+//           width: 1100,
+//           height: 800,
+//           color: Colors.purple,
+//           // child: ,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyWidget());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(child: Text('This is my First App')),
+          title: const Center(child: Text('i_am _paalwzasha')),
         ),
         body: Container(
           width: 1100,
-          height: 800,
-          color: Colors.purple,
-          // child: ,
+          height: 1100,
+          color: Colors.amber,
+          child: Column(
+            children: [
+              const Center(child: Text('Home Page')),
+              Row(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: TextButton(onPressed: () {}, child: Text('Click')),
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: Icon(
+                        Icons.star,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      width: 200,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius:  ,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
